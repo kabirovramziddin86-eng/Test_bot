f0b6a2be7c231675084a0236ee3f38516eaebd44
kabirovramziddin86-eng <kabirovramziddin86@gmail.com>
2026-01-07 20:31:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,33 @@
 import telebot
 import os
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 TOKEN = os.environ.get("BOT_TOKEN")
-
-if not TOKEN:
-    print("BOT_TOKEN topilmadi")
-    exit(1)
+PORT = int(os.environ.get("PORT", 4000))
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(
-        message,
-        "Salom 👋\nQanday yordam bera olaman?"
-    )
+    bot.reply_to(message, "Salom 👋\nQanday yordam bera olaman?")
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda m: True)
 def echo(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, m.text)
+
+# ----- FAKE WEB SERVER -----
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_server():
+    server = HTTPServer(("", PORT), SimpleHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_server).start()
 
 print("Bot ishga tushdi...")
 bot.infinity_polling()
